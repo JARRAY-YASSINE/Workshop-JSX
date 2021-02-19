946d18d5a26ab95537a568bf87902df9e5374fa8
JARRAY-YASSINE <62037959+JARRAY-YASSINE@users.noreply.github.com>
2021-02-19 23:22:39 +0200

    import image/video from Pubic & src

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
+import JSXImage from './assets/jsx-src.png'
+
+
 function App() {
+
+  const name = "Love react by 2021"
+  const myStyle = {
+    color: "blue"
+  }
+
   return (
+
+    /**
+     * JSX code
+     */
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p className="test-paragraph">Type ur name here ... export default Yassine</p>
+
+      {/* use expression to view data */}
+      <p style={{ color: "green", margin: "3 30%", backgroundColor: "yellow" }}>{name}</p>
+      <p style={myStyle}>{6 * 3.202}</p>
+
+      {/* Closing tags */}
+      <hr style={{ width: "50%" }} />
+
+      {/* Warning without the "alt" prop of "img" tag */}
+      <img
+        className="react-logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png"
+        alt="logo react"
+      />
+      <br />
+
+      {/* importing images */}
+      <img src="logo192.png" alt="" style={{ width: "20%" }} />
+      <br /><br /><br />
+      <img src={JSXImage} alt="" style={{ width: "10%" }} />
+      <br /><br /><br />
+      <video controls src={"/assets/WhySelenium.mp4"} type={"video/mp4"} width={"320"} height={"240"}/>  
+      <br /><br /><br />
+      
+      
     </div>
   );
 }
