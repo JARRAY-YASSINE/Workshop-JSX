b1baa4bd4d1f5af9c788bd2beafdeb22216e5fcd
JARRAY-YASSINE <62037959+JARRAY-YASSINE@users.noreply.github.com>
2021-02-22 10:29:59 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -20,16 +20,16 @@ function App() {
       <p className="test-paragraph">Type ur name here ... export default Yassine</p>
 
       {/* use expression to view data */}
-      <p style={{ color: "green", margin: "3 30%", backgroundColor: "yellow" }}>{name}</p>
+      <p style={{ color: "red", margin: "3 30%", backgroundColor: "rgb(10, 240, 190)" }}>{name}</p>
       <p style={myStyle}>{6 * 3.202}</p>
 
       {/* Closing tags */}
       <hr style={{ width: "50%" }} />
-      
-      {/* importing images */}
+
+      {/* importing images / video*/}
       <img src='logo192.png' alt="" style={{ width: "14%" }} />
       <br /><br /><br />
-      <img src='./src/assets/jsxsrc.png' alt="" style={{ width: "20%" }} />
+      <img src='/src/assets/jsxSrc.png' alt="" style={{ width: "20%" }} />
       <br /><br /><br />
       <video controls src={"/assets/WhySelenium.mp4"} type={"video/mp4"} width={"320"} height={"240"}/>  
       <br /><br /><br />
